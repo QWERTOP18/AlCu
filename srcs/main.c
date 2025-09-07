4203fa51a9903be96ff72ce0630f550ae93ecc1c
QWERTOP18 <mz18yui@gmail.com>
2025-09-07 14:49:25 +0900

    Refactor error handling function in main.c: moved definition to the end of the file for better readability and maintainability.

--- srcs/main.c
+++ srcs/main.c
@@ -6,25 +6,13 @@
 /*   By: yui <yui@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 23:58:46 by ymizukam          #+#    #+#             */
-/*   Updated: 2025/09/07 14:35:52 by yui              ###   ########.fr       */
+/*   Updated: 2025/09/07 14:42:29 by yui              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "interface.h"
 
-
-static int err(t_map *map)
-{
-	int status;
-	status = EXIT_FAILURE;
-	if (map)
-	{
-		status = map->errno;
-		free(map);
-	}
-	write(STDERR_FILENO, "Error\n", 6);
-	return (status);
-}
+static int	err(t_map *map);
 
 int	main(void)
 {
@@ -38,4 +26,17 @@ int	main(void)
 	}
 	console_map(map);
 	return (EXIT_SUCCESS);
+}
+
+static int err(t_map *map)
+{
+	int status;
+	status = EXIT_FAILURE;
+	if (map)
+	{
+		status = map->errno;
+		free(map);
+	}
+	write(STDERR_FILENO, "Error\n", 6);
+	return (status);
 }
